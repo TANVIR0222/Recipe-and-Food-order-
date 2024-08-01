b7bd8069ebb758f318ac762f891762e93cf6b0ed
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-02 00:34:26 +0600

    get data Databae mongodb

--- src/components/pages/Home/CatagoryWarpper/CategoryPage.jsx
+++ src/components/pages/Home/CatagoryWarpper/CategoryPage.jsx
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom";
+import CatagoryWarpper from "./CatagoryWarpper";
 
 const CategoryPage = () => {
     const{category} = useParams();
     return (
         <div className="px-6 lg:py-12 py-20">
+           <h1 className=" capitalize text-center text-3xl py-10 font-semibold text-black sm:text-6xl sm:leading-relaxed">
            {category}
+           </h1>
+           <CatagoryWarpper/>
         </div>
     );
 };
