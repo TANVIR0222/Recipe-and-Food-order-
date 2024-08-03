4405ed6d8e64bd19e524ad9f4ba94910f6030c97
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-03 23:25:20 +0600

    weekly new and name & email

---
+++ src/components/pages/Home/Newsweek/Newsweek.jsx
@@ -0,0 +1,36 @@
+
+const Newsweek = () => {
+  return (
+    <div className="text-center  my-52">
+      <div>
+        <h1 className="text-xl md:text-3xl my-8 text-black">Sing up for my weekly newsletter!</h1>
+        <p className="text-gray-500 my-4">
+          Weekly email with my latest recipe cooking tips and tricks and product
+          recommendations! <br /> You'll be set up in minutes
+        </p>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className=" mx-4 grid-cols-1">
+          <input
+            className="w-96 my-3 text-black mx-2 h-16 rounded-md   bg-slate-100 "
+            type="text"
+            placeholder="Name"
+            required
+            color="gray"
+          />
+          <input
+            className="w-96 my-3 mx-2 h-16 rounded-md  bg-slate-100"
+            type="email"
+            placeholder="Email"
+            required
+            color="gray"
+          />
+          <button className="bg-blue-600 text-white p-5 rounded-md">Get Started</button>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Newsweek;
