21a649793b025573daa00d4d151d67e7fba62c5f
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-10 22:55:50 +0600

    add AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./Routes/Router";
+import AuthProvider from "./components/provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-screen-xl	 mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <AuthProvider>
+      <div className="max-w-screen-xl	 mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>
 );
