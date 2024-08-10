00ef3c353d5061d06dd4310183d357b8446154bc
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-10 22:56:51 +0600

    paginations

--- src/components/pages/Order/ViewCard/OrdeFood.jsx
+++ src/components/pages/Order/ViewCard/OrdeFood.jsx
@@ -2,16 +2,20 @@ import useOrder from "../../../../Hooks/useOrder";
 import OrderCards from "../OrderCards/OrderCards";
 
 const OrdeFood = () => {
-    const [orders] = useOrder();
-    return (
-        <div className="grid md:grid-cols-4">
-            {
-                orders.map((items) =>
-                    <OrderCards key={items._id} items={items}></OrderCards>
-                )
-            }
-        </div>
-    );
+  const [orders] = useOrder();
+
+  return (
+    <div className="">
+      <div className="grid md:grid-cols-4">
+        {orders.map((items) => (
+          <OrderCards key={items._id} items={items}></OrderCards>
+        ))}
+      </div>
+      <div className="">
+        
+      </div>
+    </div>
+  );
 };
 
 export default OrdeFood;
