50dc9d6c32dca03a1a1516b3d97f1fadbcbf98da
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-11 16:08:37 +0600

    add carts icons

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,6 +2,10 @@ import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
 
 const Header = () => {
   const usePath = useLocation().pathname;
@@ -14,6 +18,14 @@ const Header = () => {
       console.log(err);
     })
   }
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "0 4px",
+    },
+  }));
   return (
     <div className="">
       <Navbar fluid rounded className="bg-slate-200 z-10">
@@ -35,6 +47,14 @@ const Header = () => {
         <div className="flex md:order-2">
         {
           user? <>
+          <div className="flex items-center mr-8 md:mr-16 justify-center">
+          <IconButton aria-label="cart">
+            <StyledBadge badgeContent={`10`} color="secondary" >
+              <ShoppingCartIcon color="red"   />
+            </StyledBadge>
+          </IconButton>
+          </div>
+
           <Dropdown
           arrowIcon={false}
           inline
@@ -90,6 +110,7 @@ const Header = () => {
           >
             <Link to={"/contact"}>Contact</Link>
           </Navbar.Link>
+          
         </Navbar.Collapse>
       </Navbar>
     </div>
