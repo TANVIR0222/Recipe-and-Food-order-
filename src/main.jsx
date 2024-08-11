7fddad51d18f80e21f4cf61d2d800ce52c727668
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-11 23:23:34 +0600

    tan stack query add

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./Routes/Router";
 import AuthProvider from "./components/provider/AuthProvider";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <div className="max-w-screen-xl	 mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <div className="max-w-screen-xl	 mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
