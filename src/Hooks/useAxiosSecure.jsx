aae04dd1fb17992cfef05d63f777db2f2857fd92
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-13 22:44:05 +0600

    secret data send this api

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,13 +1,44 @@
-
-import axios from 'axios'
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000/',
-})
+  baseURL: "http://localhost:3000/",
+});
 
 const useAxiosSecure = () => {
-    return axiosSecure;
+    const {logout} = useAuth();
+    const navigate = useNavigate();
+  // Add a request interceptor
+  axiosSecure.interceptors.request.use(
+    function (config) {
+        const token = localStorage.getItem('access-token')
+        config.headers.authorization = `Bearer ${token}`
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    });
+
+    axiosSecure.interceptors.response.use(function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+      }, async (error) => {
+        const status = error.response.status;
+        // for 401 or 403 logOut the user and move the user to login 
+        if(status === 401 || status === 403){
+             await logout();
+            navigate('/singin')
+        }
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+    });
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
