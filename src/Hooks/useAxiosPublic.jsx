8be2a57803bec933fa186dc4c2885c0b669d04ca
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-09-05 20:41:51 +0600

    bug fiexd

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3000/:",
-=======
-  baseURL: "https://raf-server-five.vercel.app/",
->>>>>>> 54031d8457c0823e96f6231347123a2790e7dbee
+  baseURL: "http://localhost:3000/",
 });
 
 const useAxiosPublic = () => {
